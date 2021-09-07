0de5d03e88c2f647460836551dadeffc6b578f2a
Jayanth9710 <kumarjayanth55@gmail.com>
2021-09-07 14:13:15 +0530

    typeerror

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
     const getPins = async () => {
       try {
         const allPins = await axios.get("/pins");
-        setPins(allPins.data);
+        setPins([...allPins.data]);
       } catch (error) {
         console.log(error);
       }
