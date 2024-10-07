8b737aff60af45be5cb11799436c07ddf09fcfbb
Jayanth9710 <68297123+Jayanth9710@users.noreply.github.com>
2024-10-07 20:34:02 +0530

    Update settings.js

--- src/settings.js
+++ src/settings.js
@@ -2,7 +2,7 @@ let getSettings = () => {
     return {
         // api:"http://localhost:8800/api"
         // api : "https://foodiesplore.herokuapp.com/api"
-        api : "https://jaymap-backend.vercel.app/?vercelToolbarCode=VWHSHwHDUbIC9s5"
+        api : "https://jaymap-backend-jayanth9710s-projects.vercel.app/"
     }
 }
 
